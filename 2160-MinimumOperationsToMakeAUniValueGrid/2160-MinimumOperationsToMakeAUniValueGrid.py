04387bf1f3fdbc11afa78ec9c9eecd1730721413
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:40:36 +0530

    Create 2160-MinimumOperationsToMakeAUniValueGrid

---
+++ 2160-MinimumOperationsToMakeAUniValueGrid/2160-MinimumOperationsToMakeAUniValueGrid.py
@@ -0,0 +1,13 @@
+# Last updated: 7/5/2026, 7:40:35 PM
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        nums = []
+        mod = grid[0][0] % x
+        for row in grid:
+            for v in row:
+                if v % x != mod:
+                    return -1
+                nums.append(v)
+        nums.sort()
+        mid = nums[len(nums) >> 1]
+        return sum(abs(v - mid) // x for v in nums)
