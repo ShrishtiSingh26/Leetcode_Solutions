9206ea3685bfce1b1f56385a847fb8a0cc11d8d6
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:40:04 +0530

    Create 3250-MaximumSquareAreaByRemovingFencesFromAField

---
+++ 3250-MaximumSquareAreaByRemovingFencesFromAField/3250-MaximumSquareAreaByRemovingFencesFromAField.py
@@ -0,0 +1,28 @@
+# Last updated: 7/5/2026, 7:40:03 PM
+class Solution(object):
+    def maximizeSquareArea(self, m, n, hFences, vFences):
+        MOD = 10**9 + 7
+
+        # Add boundary fences
+        h = sorted(hFences + [1, m])
+        v = sorted(vFences + [1, n])
+
+        # All possible horizontal distances
+        h_dist = set()
+        for i in range(len(h)):
+            for j in range(i + 1, len(h)):
+                h_dist.add(h[j] - h[i])
+
+        # All possible vertical distances
+        v_dist = set()
+        for i in range(len(v)):
+            for j in range(i + 1, len(v)):
+                v_dist.add(v[j] - v[i])
+
+        # Common distances
+        common = h_dist & v_dist
+        if not common:
+            return -1
+
+        side = max(common)
+        return (side * side) % MOD
