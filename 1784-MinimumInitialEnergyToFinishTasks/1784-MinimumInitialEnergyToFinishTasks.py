20c31507edc38cdc1761cfd63e27ba486c26adee
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:41:02 +0530

    Create 1784-MinimumInitialEnergyToFinishTasks

---
+++ 1784-MinimumInitialEnergyToFinishTasks/1784-MinimumInitialEnergyToFinishTasks.py
@@ -0,0 +1,14 @@
+# Last updated: 7/5/2026, 7:41:01 PM
+class Solution:
+  def minimumEffort(self, tasks: list[list[int]]) -> int:
+    ans = 0
+    prevSaved = 0
+
+    for actual, minimum in sorted(tasks, key=lambda x: x[0] - x[1]):
+      if prevSaved < minimum:
+        ans += minimum - prevSaved
+        prevSaved = minimum - actual
+      else:
+        prevSaved -= actual
+
+    return ans
