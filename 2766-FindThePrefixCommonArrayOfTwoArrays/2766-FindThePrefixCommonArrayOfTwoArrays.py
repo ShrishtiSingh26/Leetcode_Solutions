6a24856989a9dda8156d9d385fc81ca2e0694836
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:40:21 +0530

    Create 2766-FindThePrefixCommonArrayOfTwoArrays

---
+++ 2766-FindThePrefixCommonArrayOfTwoArrays/2766-FindThePrefixCommonArrayOfTwoArrays.py
@@ -0,0 +1,18 @@
+# Last updated: 7/5/2026, 7:40:20 PM
+class Solution:
+  def findThePrefixCommonArray(self, A: list[int], B: list[int]) -> list[int]:
+    n = len(A)
+    prefixCommon = 0
+    ans = []
+    count = [0] * (n + 1)
+
+    for a, b in zip(A, B):
+      count[a] += 1
+      if count[a] == 2:
+        prefixCommon += 1
+      count[b] += 1
+      if count[b] == 2:
+        prefixCommon += 1
+      ans.append(prefixCommon)
+
+    return ans
