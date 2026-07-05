e8add3de433db9f619eb2e9c20323b3360abaf44
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:40:29 +0530

    Create 2582-MinimumScoreOfAPathBetweenTwoCities

---
+++ 2582-MinimumScoreOfAPathBetweenTwoCities/2582-MinimumScoreOfAPathBetweenTwoCities.py
@@ -0,0 +1,22 @@
+# Last updated: 7/5/2026, 7:40:28 PM
+class Solution:
+  def minScore(self, n: int, roads: list[list[int]]) -> int:
+    ans = math.inf
+    graph = [[] for _ in range(n + 1)]  # graph[u] := [(v, distance)]
+    q = collections.deque([1])
+    seen = {1}
+
+    for u, v, distance in roads:
+      graph[u].append((v, distance))
+      graph[v].append((u, distance))
+
+    while q:
+      u = q.popleft()
+      for v, d in graph[u]:
+        ans = min(ans, d)
+        if v in seen:
+          continue
+        q.append(v)
+        seen.add(v)
+
+    return ans
