27f3c84b3e376459750061fe1f7dcfe5eebe5953
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:53:08 +0530

    File updated at 7/5/2026, 7:53:07 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,12 +1,9 @@
-# Last updated: 7/5/2026, 7:42:29 PM
-class Solution(object):
-    def twoSum(self, nums, target):
-        seen = {}  # value -> index
-
-        for i, num in enumerate(nums):
-            complement = target - num
-
-            if complement in seen:
-                return [seen[complement], i]
-
-            seen[num] = i
+# Last updated: 7/5/2026, 7:53:07 PM
+1class Solution:
+2  def twoSum(self, nums: list[int], target: int) -> list[int]:
+3    numToIndex = {}
+4
+5    for i, num in enumerate(nums):
+6      if target - num in numToIndex:
+7        return numToIndex[target - num], i
+8      numToIndex[num] = i
