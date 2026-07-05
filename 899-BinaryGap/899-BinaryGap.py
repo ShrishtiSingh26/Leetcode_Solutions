c2c35da6dffccfb5d533179faab6e41eda4e6b06
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:41:44 +0530

    Create 899-BinaryGap

---
+++ 899-BinaryGap/899-BinaryGap.py
@@ -0,0 +1,16 @@
+# Last updated: 7/5/2026, 7:41:43 PM
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        last_pos = -1
+        max_gap = 0
+        pos = 0
+        
+        while n > 0:
+            if n & 1:
+                if last_pos != -1:
+                    max_gap = max(max_gap, pos - last_pos)
+                last_pos = pos
+            n >>= 1
+            pos += 1
+        
+        return max_gap
