592a4367318da22a83b9aeb2a438c051ca09b418
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:41:47 +0530

    Create 815-ChampagneTower

---
+++ 815-ChampagneTower/815-ChampagneTower.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/5/2026, 7:41:46 PM
+class Solution {
+ public:
+  double champagneTower(int poured, int query_row, int query_glass) {
+    vector<double> dp(query_row + 1);
+    dp[0] = poured;
+
+    for (int i = 0; i < query_row; ++i) {
+      vector<double> newDp(query_row + 1);
+      for (int j = 0; j <= i; ++j)
+        if (dp[j] > 1) {
+          newDp[j] += (dp[j] - 1) / 2.0;
+          newDp[j + 1] += (dp[j] - 1) / 2.0;
+        }
+      dp = std::move(newDp);
+    }
+
+    return min(1.0, dp[query_glass]);
+  }
+};
