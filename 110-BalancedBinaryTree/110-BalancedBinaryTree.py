dd2a29d4a36435e91aecce88d3516e23e56b07c9
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-05 19:42:12 +0530

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.py
@@ -0,0 +1,26 @@
+# Last updated: 7/5/2026, 7:42:11 PM
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+
+        def height(node):
+            if not node:
+                return 0
+
+            left = height(node.left)
+            if left == -1:
+                return -1
+
+            right = height(node.right)
+            if right == -1:
+                return -1
+
+            if abs(left - right) > 1:
+                return -1
+
+            return max(left, right) + 1
+
+        return height(root) != -1
