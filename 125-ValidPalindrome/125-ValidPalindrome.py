f2566db31f7c165c9db2a09a1515f2c28e70fcda
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-06 11:54:11 +0530

    File updated at 7/6/2026, 11:54:10 AM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,8 +1,17 @@
-# Last updated: 7/6/2026, 11:08:47 AM
+# Last updated: 7/6/2026, 11:54:10 AM
 1class Solution:
-2    def isPalindrome(self, s: str) -> bool:
-3        string="".join(filter(str.isalnum,s)).lower()
-4        if string==string[::-1]:
-5            return True
-6        else:
-7            return False
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        dict1={}
+4        l=len(nums)
+5        for i in range(l):
+6            dict1[i]=nums[i]
+7        nums.sort()
+8        i=0
+9        j=l-1
+10        while i<j:
+11            if nums[i]+nums[j]==target:
+12                return [k for k, v in dict1.items() if v==nums[i] or v==nums[j] ]
+13            elif nums[i]+nums[j]>target:
+14                j-=1
+15            elif nums[i]+nums[j]<target:
+16                i+=1
