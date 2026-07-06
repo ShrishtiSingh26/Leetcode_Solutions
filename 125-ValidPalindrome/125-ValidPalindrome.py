fee5ebc507fd0f89223adad0b5c10c907f3a911c
Shrishti Singh <142707684+ShrishtiSingh26@users.noreply.github.com>
2026-07-06 11:08:48 +0530

    File updated at 7/6/2026, 11:08:47 AM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,8 +1,8 @@
-# Last updated: 7/5/2026, 7:42:06 PM
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        new="".join(filter(str.isalnum,s)).lower()
-        if new==new[::-1]:
-            return True
-        else:
-            return False
+# Last updated: 7/6/2026, 11:08:47 AM
+1class Solution:
+2    def isPalindrome(self, s: str) -> bool:
+3        string="".join(filter(str.isalnum,s)).lower()
+4        if string==string[::-1]:
+5            return True
+6        else:
+7            return False
